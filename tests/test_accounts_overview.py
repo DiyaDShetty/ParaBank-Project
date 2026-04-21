ee7a11d13c1c48d16505cb4c29c039969fcf9f11
Diya Shetty <diya.shetty@mphasis.com>
2026-04-21 14:13:30 +0530

    gitignore modified

--- tests/test_accounts_overview.py
+++
@@ -1,8 +0,0 @@
-from pages.login_page import LoginPage
-from pages.accounts_page import AccountsPage
-
-def test_accounts_overview(driver):
-    LoginPage(driver).login("john", "demo")
-
-    accounts = AccountsPage(driver)
-    assert accounts.accounts_loaded()
